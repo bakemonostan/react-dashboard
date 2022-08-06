2f09778a20029a8a721fcbb904541239ea79e46a
bakemonostan <ehizomokhomion@gmail.com>
2022-08-06 16:01:42 -0700

    paxckages installed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import './App.css';
+
 const App = () => {
   return (
     <div>
-      <h1>Hello Pluto!</h1>
+      <h1 className='text-3xl font-bold underline text-center'>Hello World</h1>
     </div>
   );
 };
